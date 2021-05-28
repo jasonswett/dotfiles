afbd12d33d33a473b5ccec2b5ad80a843effb47a
Jason Swett <jasonswett@jasons-mbp.home>
2021-05-28 14:05:12 -0400

    Add tmux line.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+tmux source-file ~/.tmux.conf
