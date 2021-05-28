6a078cdcd93b2dd24bcec8c562caa8942d55ac61
Jason Swett <jasonswett@jasons-mbp.home>
2021-05-28 14:12:44 -0400

    Misc.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
